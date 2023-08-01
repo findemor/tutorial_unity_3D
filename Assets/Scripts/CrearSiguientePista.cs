ffdad2792ee73adc4b10524b593b7b6d6d66bcdf
Findemor <me@findemor.es>
2023-08-01 20:43:24 +0200

    instanciar pistas

---
+++ Assets/Scripts/CrearSiguientePista.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearSiguientePista : MonoBehaviour
+{
+    public GameObject PrefabPista;
+
+    public Transform PistaPadre;
+    private Vector3 SiguientePosicion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SiguientePosicion = new Vector3(PistaPadre.position.x - PistaPadre.localScale.x, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Jugador")
+        {
+            Debug.Log("Jugador ha entrado en el trigger");
+            Instantiate(PrefabPista, SiguientePosicion, Quaternion.identity);
+        }
+    }
+
+}
